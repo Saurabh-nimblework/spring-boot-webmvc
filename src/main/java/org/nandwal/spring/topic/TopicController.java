e07d48d7d187dfbb830d6e6573d08fe436ae5a88
Saurabh Nandwal <saurabh.nandwal@nimblework.com>
2024-12-06 02:50:22 +0530

    use template for boilerplate code

--- src/main/java/org/nandwal/spring/topic/TopicController.java
+++ src/main/java/org/nandwal/spring/topic/TopicController.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.async.DeferredResult;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 @Controller
 public class TopicController {
@@ -19,131 +19,90 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
-    @RequestMapping("/topics")
-    public DeferredResult<ResponseEntity<List<Topic>>> getAllTopics(Model model) {
-        DeferredResult<ResponseEntity<List<Topic>>> deferredResult = new DeferredResult<>();
+    private <T> DeferredResult<ResponseEntity<T>> createDeferredResult(Consumer<DeferredResult<ResponseEntity<T>>> task) {
+        DeferredResult<ResponseEntity<T>> deferredResult = new DeferredResult<>();
         deferredResult.onError((error) -> {
             deferredResult.setErrorResult(ResponseEntity.status(500).body("Error occurred!"));
         });
         deferredResult.onTimeout(() -> {
             deferredResult.setErrorResult(ResponseEntity.status(408).body("Request timed out!"));
         });
-
-        topicService.getAllTopics()
-                .thenAccept(topics -> {
-                    deferredResult.setResult(new ResponseEntity<>(topics, HttpStatus.OK));
-                })
-                .exceptionally(ex -> {
-                    Map<String, Object> error = new HashMap<>();
-                    error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-                    error.put("message", ex.getMessage());
-                    deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
-                    return null;
-                });
-
+        task.accept(deferredResult);
         return deferredResult;
+    }
+
+    @RequestMapping("/topics")
+    public DeferredResult<ResponseEntity<List<Topic>>> getAllTopics(Model model) {
+        return createDeferredResult(deferredResult ->
+                topicService.getAllTopics()
+                        .thenAccept(topics -> deferredResult.setResult(new ResponseEntity<>(topics, HttpStatus.OK)))
+                        .exceptionally(ex -> {
+                            Map<String, Object> error = new HashMap<>();
+                            error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+                            error.put("message", ex.getMessage());
+                            deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
+                            return null;
+                        })
+        );
     }
 
     @RequestMapping("/topics/{idx}")
     public DeferredResult<ResponseEntity<Topic>> getTopic(@PathVariable("idx") String id, Model model) {
-        DeferredResult<ResponseEntity<Topic>> deferredResult = new DeferredResult<>();
-        deferredResult.onError((error) -> {
-            deferredResult.setErrorResult(ResponseEntity.status(500).body("Error occurred!"));
-        });
-        deferredResult.onTimeout(() -> {
-            deferredResult.setErrorResult(ResponseEntity.status(408).body("Request timed out!"));
-        });
-
-        topicService.getTopic(id)
-                .thenAccept(topics -> {
-                    deferredResult.setResult(new ResponseEntity<>(topics, HttpStatus.OK));
-                })
-                .exceptionally(ex -> {
-                    Map<String, Object> error = new HashMap<>();
-                    error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-                    error.put("message", ex.getMessage());
-                    deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
-                    return null;
-                });
-
-        return deferredResult;
+        return createDeferredResult(deferredResult ->
+                topicService.getTopic(id)
+                        .thenAccept(topic -> deferredResult.setResult(new ResponseEntity<>(topic, HttpStatus.OK)))
+                        .exceptionally(ex -> {
+                            Map<String, Object> error = new HashMap<>();
+                            error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+                            error.put("message", ex.getMessage());
+                            deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
+                            return null;
+                        })
+        );
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/topics")
     public DeferredResult<ResponseEntity<Void>> addTopic(@RequestBody Topic topic) {
-        DeferredResult<ResponseEntity<Void>> deferredResult = new DeferredResult<>();
-        deferredResult.onError((error) -> {
-            deferredResult.setErrorResult(ResponseEntity.status(500).body("Error occurred!"));
-        });
-        deferredResult.onTimeout(() -> {
-            deferredResult.setErrorResult(ResponseEntity.status(408).body("Request timed out!"));
-        });
-
-        topicService.addTopic(topic)
-                .thenRun(() -> {
-                    deferredResult.setResult(new ResponseEntity<>(HttpStatus.CREATED));
-                })
-                .exceptionally(ex -> {
-                    Map<String, Object> error = new HashMap<>();
-                    error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-                    error.put("message", ex.getMessage());
-                    deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
-                    return null;
-                });
-
-        return deferredResult;
+        return createDeferredResult(deferredResult ->
+                topicService.addTopic(topic)
+                        .thenRun(() -> deferredResult.setResult(new ResponseEntity<>(HttpStatus.CREATED)))
+                        .exceptionally(ex -> {
+                            Map<String, Object> error = new HashMap<>();
+                            error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+                            error.put("message", ex.getMessage());
+                            deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
+                            return null;
+                        })
+        );
     }
 
-    @RequestMapping(method= RequestMethod.PUT, value="/topics/{id}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
     public DeferredResult<ResponseEntity<Void>> updateTopic(@RequestBody Topic topic, @PathVariable String id, Model model) {
-        DeferredResult<ResponseEntity<Void>> deferredResult = new DeferredResult<>();
-        deferredResult.onError((error) -> {
-            deferredResult.setErrorResult(ResponseEntity.status(500).body("Error occurred!"));
-        });
-        deferredResult.onTimeout(() -> {
-            deferredResult.setErrorResult(ResponseEntity.status(408).body("Request timed out!"));
-        });
-        topicService.updateTopic(topic, id)
-                .thenRun(() -> {
-                    deferredResult.setResult(new ResponseEntity<>(HttpStatus.OK));
-                })
-                .exceptionally(ex -> {
-                    Map<String, Object> error = new HashMap<>();
-                    error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-                    error.put("message", ex.getMessage());
-                    deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
-                    return null;
-                });
-        return deferredResult;
+        return createDeferredResult(deferredResult ->
+                topicService.updateTopic(topic, id)
+                        .thenRun(() -> deferredResult.setResult(new ResponseEntity<>(HttpStatus.OK)))
+                        .exceptionally(ex -> {
+                            Map<String, Object> error = new HashMap<>();
+                            error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+                            error.put("message", ex.getMessage());
+                            deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
+                            return null;
+                        })
+        );
     }
 
-    @RequestMapping(method= RequestMethod.DELETE, value="/topics/{id}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
     public DeferredResult<ResponseEntity<Void>> deleteTopic(@PathVariable String id, Model model) {
-        DeferredResult<ResponseEntity<Void>> deferredResult = new DeferredResult<>();
-        deferredResult.onError((error) -> {
-            deferredResult.setErrorResult(ResponseEntity.status(500).body("Error occurred!"));
-        });
-        deferredResult.onTimeout(() -> {
-            deferredResult.setErrorResult(ResponseEntity.status(408).body("Request timed out!"));
-        });
-        topicService.deleteTopic(id)
-                .thenRun(() -> {
-                    deferredResult.setResult(new ResponseEntity<>(HttpStatus.NO_CONTENT));
-                })
-                .exceptionally(ex -> {
-                    Map<String, Object> error = new HashMap<>();
-                    error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-                    error.put("message", ex.getMessage());
-                    deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
-                    return null;
-                })
-                .exceptionally(ex -> {
-                    Map<String, Object> error = new HashMap<>();
-                    error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-                    error.put("message", ex.getMessage());
-                    deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
-                    return null;
-                });
-        return deferredResult;
+        return createDeferredResult(deferredResult ->
+                topicService.deleteTopic(id)
+                        .thenRun(() -> deferredResult.setResult(new ResponseEntity<>(HttpStatus.NO_CONTENT)))
+                        .exceptionally(ex -> {
+                            Map<String, Object> error = new HashMap<>();
+                            error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+                            error.put("message", ex.getMessage());
+                            deferredResult.setErrorResult(new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR));
+                            return null;
+                        })
+        );
     }
 }
